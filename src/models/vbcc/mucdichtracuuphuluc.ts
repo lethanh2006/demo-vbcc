f8a7fd6e4bd852a48fbf9ae828efcb2198da4404
TVHuan <tahuan1906@gmail.com>
2025-07-01 09:01:14 +0700

    fix

--- src/models/vbcc/mucdichtracuuphuluc.ts
+++ src/models/vbcc/mucdichtracuuphuluc.ts
@@ -1,18 +1,11 @@
 import useInitModel from '@/hooks/useInitModel';
-import { ip3 } from '@/utils/ip';
 
 const initSort = {
 	soThuTu: 1 as 1 | -1, // Sắp xếp tăng dần theo trường 'soThuTu'
 };
 
 export default () => {
-	const objInit = useInitModel<MucDichTraCuuPhuLuc.IRecord>(
-		'muc-dich-tra-cuu-phu-luc',
-		undefined,
-		undefined,
-		ip3,
-		initSort,
-	);
+	const objInit = useInitModel<MucDichTraCuuPhuLuc.IRecord>('muc-dich-tra-cuu-phu-luc', undefined, initSort);
 
 	return {
 		...objInit,
