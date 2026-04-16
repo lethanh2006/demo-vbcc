98be20a509f9a64c3f624e0471f781fbce296e17
Phạm Đức <p.mduc.ptit@gmail.com>
2026-04-16 09:03:56 +0700

    QLPhoiBang, fix UI, fix margin in bt

--- src/pages/QuanLyPhoiBang/components/TabBieuMauPhoiBang.tsx
+++ src/pages/QuanLyPhoiBang/components/TabBieuMauPhoiBang.tsx
@@ -257,12 +257,12 @@ const TabBieuMauPhoiBang = () => {
 				)}
 
 				<Col span={24}>
-					<Form.Item label={intl.formatMessage({ id: 'phoibang.column.ghichu' })} name='ghiChu'>
+					<Form.Item label={intl.formatMessage({ id: 'phoibang.column.ghichu' })} name='ghiChu' style={{ marginBottom: 0 }}>
 						<Input.TextArea rows={2} placeholder={intl.formatMessage({ id: 'phoibang.placeholder.ghichu' })} />
 					</Form.Item>
 				</Col>
 			</Row>
-			<div className='form-footer' style={{ marginTop: 24 }}>
+			<div className='form-footer' style={{ marginTop: 12, marginBottom: -12 }}>
 				<Button loading={formSubmiting} htmlType='submit' type='primary'>
 					{!edit
 						? `${intl.formatMessage({ id: 'global.button.themmoi' })}`
